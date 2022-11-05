2bd01702a9422fe8080c9d28ab4c9cfb6780302f
murekswork <murekswork@gmail.com>
2022-11-06 00:54:00 +0400

    added profiles_likes urls

--- dating_logic/urls.py
+++ dating_logic/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('match/<int:user2>/<str:reaction>', LikeView, name='like'),
+    path('match/<int:action_user_id>/<str:reaction>', LikeView, name='like'),
     path('matches/', MatchesPageView, name='matches'),
+    path('profile_likes/', WhoLikedView, name='profile_likes'),
+    path('profile_likes/<int:action_user_id>/<str:reaction>/', ReactLikeView, name='react_like'),
     path('dates/', DatesPageView, name='dates'),
     path('', HomePageView, name='home'),
 ]
